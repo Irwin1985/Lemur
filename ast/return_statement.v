aedc93abeea2caa63b682da031a83c9a5e54ecf0
Irwin <rodriguez.irwin@gmail.com>
2021-11-21 23:12:58 +0100

    remove unnecessary code.

--- ast/return_statement.v
+++ ast/return_statement.v
@@ -7,12 +7,6 @@ pub struct ReturnStatement {
 		value Node
 }
 
-fn (r ReturnStatement) statement_node() {}
-
-fn (r ReturnStatement) token_literal() string {
-	return r.token.lexeme
-}
-
 pub fn (r ReturnStatement) to_string() string {
 	return r.value.to_string()
 }
--- ast/string.v
+++ ast/string.v
@@ -7,12 +7,6 @@ pub struct StringLiteral {
 		value string
 }
 
-fn (s StringLiteral) expression_node() {}
-
-fn (s StringLiteral) token_literal() string {
-	return s.token.lexeme
-}
-
 pub fn (s StringLiteral) to_string() string {
 	return '"$s.value"'
 }
